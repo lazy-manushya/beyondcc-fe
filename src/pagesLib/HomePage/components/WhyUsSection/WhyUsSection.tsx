8e1cbc8dbe53f8a90ccb79e04ba9bc46726b6f54
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2023-06-27 12:52:35 +0530

    Update WhyUsSection.tsx

--- src/pagesLib/HomePage/components/WhyUsSection/WhyUsSection.tsx
+++ src/pagesLib/HomePage/components/WhyUsSection/WhyUsSection.tsx
@@ -13,7 +13,7 @@ const WhyUsSection: React.FC<IWhyUsSectionProps> = () => {
     <StyledSection>
       <StyledContainer>
         <div className="h-100">
-          <h2>Why Beyondcc? We're the Future of Skill Based Hiring</h2>
+          <h2>Why Beyondcc? We&apos;re the Future of Skill Based Hiring</h2>
           <p>
             Beyondcc is the future of objective-based hiring. We understand that
             each job requires a unique set of skills and qualifications, and
@@ -21,7 +21,7 @@ const WhyUsSection: React.FC<IWhyUsSectionProps> = () => {
           </p>
 
           <p>
-            To address this, we delve deep into job descriptions and candidates'
+            To address this, we delve deep into job descriptions and candidates&apos;
             backgrounds to provide precision matchmaking for business
             professionals. Our goal is to reduce subjectivity in hiring and
             minimize the occurrence of wrong hires, saving companies time and
