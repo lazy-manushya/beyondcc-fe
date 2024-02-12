91ba692d8b640cbe42339e0f260f76c62a54a7a9
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2024-02-12 12:39:30 +0530

    Create links.ts

---
+++ src/config/links.ts
@@ -0,0 +1,105 @@
+export const SITE_LINKS = {
+  HOME_PAGE: "/",
+  CANDIDATES_LANDING_PAGE: "#",
+  CLIENTS_LANDING_PAGE: "#",
+};
+
+export const HEADER_LINKS = [
+  {
+    content: "Explore opportunities",
+    link: "#",
+  },
+  {
+    content: "Find talent",
+    link: "#",
+  },
+  {
+    content: "Beyondcc remote",
+    link: "#",
+  },
+];
+
+export const FOOTER_LINKS = [
+  {
+    sectionTitle: "Navigation",
+    links: [
+      {
+        content: "For job seekers",
+        link: "#",
+      },
+      {
+        content: "For startups",
+        link: "#",
+      },
+      {
+        content: "Beyondcc Remote",
+        link: "#",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Join us",
+    links: [
+      {
+        content: "Sales",
+        link: "#",
+      },
+      {
+        content: "Slack community",
+        link: "#",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Recources",
+    links: [
+      {
+        content: "Blogs",
+        link: "#",
+      },
+      {
+        content: "Newsletter",
+        link: "#",
+      },
+      {
+        content: "Podcast",
+        link: "#",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Company",
+    links: [
+      {
+        content: "About us",
+        link: "#",
+      },
+      {
+        content: "Contact us",
+        link: "#",
+      },
+      {
+        content: "Privacy policy",
+        link: "#",
+      },
+    ],
+  },
+];
+
+export const SOCIAL_LINKS = [
+  {
+    title: "Youtube",
+    link: "#",
+    icon: "/assets/images/icons/social/youtube.svg",
+  },
+  {
+    content: "Linkdin",
+    link: "#",
+    icon: "/assets/images/icons/social/linkdin.svg",
+  },
+  {
+    content: "Twitter / X",
+    link: "#",
+    icon: "/assets/images/icons/social/twitter_x.svg",
+  },
+];
