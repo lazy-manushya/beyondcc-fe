c86c7d9ebd1de873e808b32f22c8eae3f83533bb
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2024-02-12 12:37:45 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
-import type { Metadata } from "next";
+"use client";
+
+// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import StyledComponentsRegistry from "~/styles/libs/Registry";
+import MainLayout from "~/features/layouts/MainLayout";
+
+import "~/styles/reset.css";
+import "~/styles/core.css";
+import "~/styles/typography.css";
+import "~/styles/color.css";
+import "~/styles/animations.css";
+import "~/styles/scroll.css";
+import "~/styles/sizing.css";
+import "~/styles/utils.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Beyond CC",
-  description: "Beyond CC",
-};
+// export const metadata: Metadata = {
+//   title: "Beyond CC",
+//   description: "Beyond CC",
+// };
 
 export default function RootLayout({
   children,
@@ -14,8 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        <StyledComponentsRegistry>
+          <MainLayout>{children}</MainLayout>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
